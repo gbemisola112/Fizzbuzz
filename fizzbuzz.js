7fae2693a9c9b177c4d050a31b08a75fb1e485bb
moboluwaduroatopo <daudamonsurat2022@gmail.com>
2023-08-22 16:13:16 +0100

    first commit

---
+++ fizzbuzz.js
@@ -0,0 +1,29 @@
+function score(){
+    for(var a=0; a <=100; a++){
+        if (a %3==0 && a %5==0) {
+            console.log("fizzbuzz");
+            show.innerHTML =  "fizzbuz "
+        
+        } 
+    } 
+    for(var b=0; b<=100; b++){
+     if (a %3==0 ) {
+     console.log("fizz");
+    show.innerHTML="this is fizz"
+        
+    }  
+}
+for(var c=0; a <=100; c++){
+        if (a %5==0 ) {
+        console.log("buzz");
+        show.innerHTML+= "this is buzz"
+    }
+}
+}
+// var number = 79398393
+// var score = 3
+// if (number % 2 == 0) {
+//     console.log("even number");
+// } else if (number % 2 != 0) {
+//     console.log("odd number");
+// }
